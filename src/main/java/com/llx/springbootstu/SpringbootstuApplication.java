35d8201ba69785077d67cea24e9687898c345d98
sharp123 <liulixia_jss@petrochina.com.cn>
2019-03-28 09:34:56 +0800

    springbootstu

---
+++ src/main/java/com/llx/springbootstu/SpringbootstuApplication.java
@@ -0,0 +1,13 @@
+package com.llx.springbootstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstuApplication.class, args);
+	}
+
+}
