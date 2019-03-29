0cc58d87e1154638fa18592ee1e7b90c792f8143
sharp123 <liulixia_jss@petrochina.com.cn>
2019-03-29 15:26:01 +0800

    使用Thymeleaf模板引擎渲染web视图使用freemarker模板引擎	使用velocity渲染web视图

---
+++ chapter3-1-2/src/main/java/com/llx/summer/Chapter312Application.java
@@ -0,0 +1,13 @@
+package com.llx.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter312Application.class, args);
+	}
+
+}
